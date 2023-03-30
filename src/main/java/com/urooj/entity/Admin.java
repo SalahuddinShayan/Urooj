c8368b934103527dea915424fca64bec48357dae
SalahuddinShayan <104673244+SalahuddinShayan@users.noreply.github.com>
2023-03-30 12:25:26 +0530

    4th commit

--- src/main/java/com/urooj/entity/Admin.java
+++ src/main/java/com/urooj/entity/Admin.java
@@ -3,14 +3,17 @@ package com.urooj.entity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="Admin")
 public class Admin {
 	
 	@Id
+	@Column(name="login")
 	private String login;
 	
-	@Column
+	@Column(name="Password")
 	private String Password;
 
 	public String getLogin() {
